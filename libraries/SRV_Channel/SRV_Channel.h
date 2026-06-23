64aabe7beeeeb3930567edf0560219d3da854385
Stefard100 <airbound@LAPTOP-OVE24MET.localdomain>
2026-06-23 23:45:23 +0530

    Tilt_Rotor + Thrust vectoring setup for V2

--- libraries/SRV_Channel/SRV_Channel.h
+++ libraries/SRV_Channel/SRV_Channel.h
@@ -219,6 +219,8 @@ public:
         k_actuator4             = 187,
         k_actuator5             = 188,
         k_actuator6             = 189,
+        k_tiltMotorLeftVec      = 190,   // vectored thrust, left tilt with independent attitude vectoring
+        k_tiltMotorRightVec     = 191,   // vectored thrust, right tilt with independent attitude vectoring
         k_nr_aux_servo_functions         ///< This must be the last enum value (only add new values _before_ this one)
     } Function;
 
